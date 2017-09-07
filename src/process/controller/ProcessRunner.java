5a0f6c5a84af4774f181fa9bffefdfd73457af7b
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2017-09-07 10:54:51 -0700

    Wrote runner

--- src/process/controller/ProcessRunner.java
+++ src/process/controller/ProcessRunner.java
@@ -2,4 +2,11 @@ package process.controller;
 
 public class ProcessRunner {
 	
+	public static void main(String[] args) {
+		
+		ProcessController process = new ProcessController();
+		process.start();
+		
+	}
+	
 }
