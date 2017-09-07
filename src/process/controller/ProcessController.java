1d93f5026c3c38195e694d1fb01ed41a9afd9172
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2017-09-07 12:51:54 -0700

    changed input.next() to input.nextLine()

--- src/process/controller/ProcessController.java
+++ src/process/controller/ProcessController.java
@@ -9,10 +9,9 @@ public class ProcessController {
 		Scanner input = new Scanner(System.in);
 		
 		System.out.print("Enter text: ");
-		String in = input.next();
+		String in = input.nextLine();
 		
 		System.out.println(in);
-		
 		
 		
 		input.close();
