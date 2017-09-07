7e43b217b644a0958a757c67bc2c928edec5572f
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2017-09-07 12:46:23 -0700

    added user input

--- src/process/controller/ProcessController.java
+++ src/process/controller/ProcessController.java
@@ -1,11 +1,21 @@
 package process.controller;
 
+import java.util.Scanner;
+
 public class ProcessController {
 	
 	public void start() {
 		
-		System.out.println("Hello");
+		Scanner input = new Scanner(System.in);
 		
+		System.out.print("Enter text: ");
+		String in = input.next();
+		
+		System.out.println(in);
+		
+		
+		
+		input.close();
 	}
 	
 }
