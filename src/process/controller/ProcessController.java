acb71f62b423e4588020b752ee8e5e8766c03c7b
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2017-09-11 11:14:29 -0700

    made a PlayDoh circle

--- src/process/controller/ProcessController.java
+++ src/process/controller/ProcessController.java
@@ -1,13 +1,16 @@
 package process.controller;
 
 import java.util.Scanner;
+import process.model.PlayDohCircle;
 
 public class ProcessController {
 	
 	public void start() {
 		
 		Scanner input = new Scanner(System.in);
+		PlayDohCircle circle = new PlayDohCircle(5);
 		
+		System.out.println("Circle size: " + circle.getSize());
 		System.out.print("Enter text: ");
 		String in = input.nextLine();
 		
---
+++ src/process/model/PlayDohCircle.java
@@ -0,0 +1,17 @@
+package process.model;
+
+public class PlayDohCircle {
+	
+	private double size;
+	
+	public PlayDohCircle(double size) {
+		
+		this.size = size;
+		
+	}
+	
+	public double getSize() {
+		return size;
+	}
+	
+}
