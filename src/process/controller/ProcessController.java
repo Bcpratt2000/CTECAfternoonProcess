6de5396c9520c8274a7b2c9c8a1f0e5d5a660a9a
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2017-09-13 12:50:14 -0700

    added condiion to check if sides <=0

--- src/process/controller/ProcessController.java
+++ src/process/controller/ProcessController.java
@@ -11,24 +11,25 @@ public class ProcessController {
 		Scanner input = new Scanner(System.in);
 		
 		// PlayDohCircle circle = new PlayDohCircle(5);
-		// System.out.println("Circle size: " + circle.getSize());
+// System.out.println("Circle size: " + circle.getSize());
 		// System.out.println(circle);
 		
 		
 		
 		Polygon shape = new Polygon(0);
 		
-		while (shape.getSides() == 0) {
+		while (shape.getSides() <= 0) {
 			try {
 				System.out.print("Enter number of sides: ");
 				shape.setSides(input.nextInt());
+				if(shape.getSides()<=0)
+					System.out.println("Sides must be greater than 0\n");
 			} catch (Exception e) {
 				System.out.println("Please enter an integer\n");
-				
 			}
-			input.nextLine();
+			input.nextLine(); //flush the line buffer
 		}
-		System.out.println("Sides: " + shape.getSides());
+		System.out.println("Sides set to " + shape.getSides());
 		
 //		System.out.print("Enter text: ");
 //		String in = input.nextLine();
